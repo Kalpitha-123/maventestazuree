c1efef7f4808f995cf8712e559e3bc1d1656465e
Kalpitha-123 <kalpitha.22cse@cambridge.edu.in>
2025-06-08 16:03:23 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/kalpzjava/App.java
@@ -0,0 +1,13 @@
+package com.kalpzjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
